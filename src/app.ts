4a0619de581e1cbb026250fff662349531bce6dd
JuBa <bazsi@klapantius.net>
2024-07-15 16:27:32 +0200

    change 'index' to 'app'

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,19 @@
-import mariadb from 'mariadb';
+import { myContainer } from "./inversify.config";
+import { IDatabase } from "./database/IDatabase";
 import { ImportFromJsonService } from "./services/ImportFromJsonService";
 
 class Application {
-    private readonly pool: mariadb.Pool = mariadb.createPool({
-        user: 'bazsi',
-        host: 'localhost',
-        password: 'izsab',
-        connectionLimit: 55
-    });
+    database: IDatabase;
+
+    constructor() {
+        this.database = myContainer.get<IDatabase>('IDatabase');    
+    }
+
     public async runAsync() {
-        // const dbInit = new DatabaseAccess(this.pool, 'helloworld');
-        // await dbInit.initAsync();
+        await this.database.warmup();
 
         const backupLoader = new ImportFromJsonService('/home/bazsi/code/helloworld/backup.json', 30);
-        const grouppedEntries = await backupLoader.groupEntries()
+        const grouppedEntries = await backupLoader.loadAndGroupAsync()
         console.log(`Loaded ${backupLoader.entries.length} measurements of ${grouppedEntries.length} sessions from backup file.`);
     }
 }
