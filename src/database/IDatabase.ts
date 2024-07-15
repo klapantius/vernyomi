01fe1a6ab6821e3f31186f53ff416d1fc7181d36
JuBa <bazsi@klapantius.net>
2024-07-15 18:21:24 +0200

    introduce DatabaseWriteOperationResult

--- src/database/IDatabase.ts
+++ src/database/IDatabase.ts
@@ -1,4 +1,20 @@
 export interface IDatabase {
+    warmup(): Promise<void>;
     query(sql: string, params?: any[]): Promise<any>;
     // Add other necessary operations like connect, insert, update, delete
+}
+
+/* this is like the return type from MariaDB's query method,
+ * but actually it is meant as a general type
+*/
+export class DatabaseWriteOperationResult {
+    public affectedRows: number;
+    public insertId: number;
+    public warningStatus: number;
+
+    constructor(affectedRows: number, insertId: number, warningStatus: number) {
+        this.affectedRows = affectedRows;
+        this.insertId = insertId;
+        this.warningStatus = warningStatus;
+    }
 }
