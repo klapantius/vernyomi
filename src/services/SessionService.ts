01f695ac825c18ddd3900db17f1120a18f842b71
JuBa <bazsi@klapantius.net>
2024-07-20 11:01:16 +0200

    chore: Remove unused code in SessionService

--- src/services/SessionService.ts
+++ src/services/SessionService.ts
@@ -1,7 +1,5 @@
 import { myContainer } from '../inversify.config';
-import { Measurement } from '../models/Measurement';
 import { SessionCreationSource } from '../models/SessionCreationSource';
-import { IMeasurementRepository } from '../repositories/interfaces/IMeasurementRepository';
 import { ISessionRepository } from '../repositories/interfaces/ISessionRepository';
 
 export class SessionService {
@@ -14,34 +12,5 @@ export class SessionService {
     public async createSessionAsync(creationSource: SessionCreationSource): Promise<number> {
         const result = await this.sessionRepository.createSessionAsync(creationSource);
         return result;
-    }
-}
-
-export class MeasurementService { // TODO: this doesn't belong here
-    measurementRepository: IMeasurementRepository;
-
-    constructor() {
-        this.measurementRepository = myContainer.get<IMeasurementRepository>('IMeasurementRepository');
-    }
-
-    public async saveMeasurementAsync(data: Measurement | { sessionId: string, sys: string, dia: string, puls: string, comment?: string }): Promise<void> {
-        let measurement: Measurement;
-
-        if (data instanceof Measurement) {
-            measurement = data;
-        } else {
-            measurement = new Measurement({
-                measurementId: null,
-                sessionId: +data.sessionId,
-                createdAt: new Date(),
-                sys: +data.sys,
-                dia: +data.dia,
-                puls: +data.puls
-                //, data.comment);
-            });
-        }
-
-        await this.measurementRepository.save(measurement);
-        return;
     }
 }
