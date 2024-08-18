f4548ed8e2edba17dad45e7adf42c83313c2dc1c
JuBa <bazsi@klapantius.net>
2024-08-18 17:46:33 +0200

    fix processing of non-distinguishing connection files

--- src/services/database/MariaDBConnectionPool.ts
+++ src/services/database/MariaDBConnectionPool.ts
@@ -10,7 +10,9 @@ export class MariaDBConnectionPool {
         if (!MariaDBConnectionPool.instance) {
             // load connection.cfg as alternative source of connection parameters
             const configContent = JSON.parse(fs.readFileSync('connection.cfg', 'utf8'));
+            console.log(`configContent: ${JSON.stringify(configContent)}`);
             const {DB_HOST, DB_USER, DB_PASSWORD, DB_NAME} = configContent; // destructure the configContent object
+            console.log(`DB_HOST: ${JSON.stringify(DB_HOST)}`);
             let connectionConfig;
             if (process.env.NODE_ENV?.toLowerCase() === 'test') {
                 connectionConfig = configContent.TEST;
@@ -18,11 +20,13 @@ export class MariaDBConnectionPool {
                 connectionConfig = configContent.PRODUCTION;
             }
 
+            console.log(`connectionConfig: ${JSON.stringify(connectionConfig)}`);
+
             MariaDBConnectionPool.instance = createPool({
-                host: connectionConfig?.DB_HOST ?? process.env.DB_HOST,
-                user: connectionConfig?.DB_USER ?? process.env.DB_USER,
-                password: connectionConfig?.DB_PASSWORD ?? process.env.DB_PASSWORD,
-                database: connectionConfig?.DB_NAME ?? process.env.DB_NAME,
+                host: connectionConfig?.DB_HOST ?? DB_HOST ?? process.env.DB_HOST,
+                user: connectionConfig?.DB_USER ?? DB_USER ?? process.env.DB_USER,
+                password: connectionConfig?.DB_PASSWORD ?? DB_PASSWORD ?? process.env.DB_PASSWORD,
+                database: connectionConfig?.DB_NAME ?? DB_NAME ?? process.env.DB_NAME,
                 connectionLimit: 10
             });
         }
